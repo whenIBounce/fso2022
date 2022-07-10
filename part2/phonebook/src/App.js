442046a4e36d0077db6d8d072d58289ef136a2ec
whenIBounce <zhan4868@umn.edu>
2022-07-10 14:21:06 +0800

    exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    const newPersons = persons.concat(personObject)
+    setPersons(newPersons)
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <div key={person.name}>{person.name}</div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
