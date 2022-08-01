8a50e81d402ee947142a39a15b832f1e2988874c
whenIBounce <zhan4868@umn.edu>
2022-08-01 23:35:41 +0800

    bloglist refactor: add utils/errorHandler.js, utils/unknownEndpoint.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,18 +5,21 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
-console.log(typeof(mongoUrl))
-console.log('the url', mongoUrl)
 mongoose
   .connect(mongoUrl)
   .then(() => logger.info('connected to MONGODB'))
   .catch(err => logger.error(err))
 
+app.use(express.json())
+app.use(middleware.requestLogger)
 app.use(cors())
 app.use('/api/blogs', blogsRouter)
-app.use(express.json())
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
 
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,15 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogsRouter.get('/', (request, response) => {
+blogsRouter.get('/', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body)
 
   blog
@@ -18,6 +19,7 @@ blogsRouter.post('/', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
