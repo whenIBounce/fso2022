de7f55f13df396ca652268012df280c2164725d3
whenIBounce <zhan4868@umn.edu>
2022-08-01 22:35:48 +0800

    bloglist refactor: index.js and app.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,50 +1,10 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
+const app = require('./app')
+const config = require('./utils/config')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-console.log(typeof(mongoUrl))
-console.log('the url', mongoUrl)
-mongoose
-.connect(mongoUrl)
-.then(() => console.log('connected to MONGODB'))
-.catch(err => console.log(err))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const PORT = config.PORT
+server.listen(PORT, () => {
+  console.log(`HTTP Server running on port ${PORT}`)
 })
