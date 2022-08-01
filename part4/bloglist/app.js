cf8746ca3a1d8cb401602ac99d88df39f641f640
whenIBounce <zhan4868@umn.edu>
2022-08-01 23:37:37 +0800

    exercise 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,9 +3,11 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+logger.info('connecting to', config.MONGODB_URI)
 
 const mongoUrl = config.MONGODB_URI
 mongoose
