5c9210441b28f3fb6abab15361beced8794282f6
whenIBounce <zhan4868@umn.edu>
2022-08-01 22:18:19 +0800

    exercise 4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,50 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+console.log(typeof(mongoUrl))
+console.log('the url', mongoUrl)
+mongoose
+.connect(mongoUrl)
+.then(() => console.log('connected to MONGODB'))
+.catch(err => console.log(err))
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
