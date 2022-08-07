c50668daf58e41fd3a1056cceb34b5e1ea6f7a69
whenIBounce <zhan4868@umn.edu>
2022-08-07 10:26:45 +0800

    exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,11 +1,24 @@
+const nodemon = require("nodemon");
+
 const dummy = () => {
 	return 1
 }
 
 const totalLikes = (blogs) => {
+	if (blogs.length === 0) return 0
 	return blogs.reduce((previousTotal, current) => previousTotal + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	if (blogs.length === 0) return undefined
+	const sortByLikes = blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
+	return {
+		title: sortByLikes[0].title,
+		author: sortByLikes[0].author,
+		likes: sortByLikes[0].likes
+	}
+}
+
 module.exports = {
-	dummy, totalLikes
+	dummy, totalLikes, favoriteBlog
 }
