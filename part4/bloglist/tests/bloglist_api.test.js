35b6b5668139c046eb9a2206c46b67a1f2b2f8a1
whenIBounce <zhan4868@umn.edu>
2022-08-08 17:27:57 +0800

    test HTTP GET works

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,17 +1,31 @@
 //import mongoose, supertest, app
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const app = require('../app')
+const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./test_helper')
 
 //get superagent with supertest(app)
 const api = supertest(app)
 
+beforeEach(async () => {
+	await Blog.deleteMany({})
+	let blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+	let promiseArray = blogObjects.map(blogObject => blogObject.save())
+	await Promise.all(promiseArray)
+}, 1000000)
+
 //start test
-test('get /api/blogs', async () => {
+test('blogs are returned as json', async () => {
 	await api
 		.get('/api/blogs')
 		.expect(200)
 		.expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('all blogs are returned', async () => {
+	const response = await api.get('/api/blogs')
+	expect(response.body).toHaveLength(helper.initialBlogs.length)
 }, 100000)
 
 //after all close the mongoose connection
