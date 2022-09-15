a081d4a4e028f4e32e9b8bb8d3e3a2bb6c05f335
whenIBounce <zhan4868@umn.edu>
2022-09-15 10:01:48 -0500

    update user_api.test.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -20,11 +20,11 @@ mongoose
 
 app.use(express.json())
 app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+//app.use(middleware.tokenExtractor)
 app.use(cors())
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs',  middleware.tokenExtractor,blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/bloglist/tests/user_api.test.js
+++ part4/bloglist/tests/user_api.test.js
@@ -4,7 +4,10 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
-
+const User = require('../models/user')
+beforeEach(async () => {
+	await User.deleteMany({})
+}, 1000000)
 //test
 describe('user administration', () => {
 	test('invalid users are not created', async () => {
@@ -21,6 +24,21 @@ describe('user administration', () => {
 		const usersAtEnd = await helper.usersInDb()
 		expect(response.body.error).toEqual('password must have at least three characters')
 		expect(usersAtBegin).toHaveLength(usersAtEnd.length)
+	}, 100000)
+
+	test('create a valid user', async () => {
+		const usersAtBegin = await helper.usersInDb()
+		const newUser = {
+			username: 'zhenlashi',
+			name: 'haoba',
+			password: '000000'
+		}
+		await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(201)
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtBegin).toHaveLength(usersAtEnd.length-1)
 	}, 100000)
 })
 //close mongoose connections
